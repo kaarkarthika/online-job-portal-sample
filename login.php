02f13e120f94a2da4d228a4bda97f5bb6e1ba1ba
karthika <45929533+kaarkarthika@users.noreply.github.com>
2019-03-28 15:07:13 +0530

    Update login.php

--- login.php
+++ login.php
@@ -38,13 +38,6 @@ if (isset($_POST['btn-login'])) {
     <link rel="stylesheet" href="assets/css/bootstrap.min.css" type="text/css"/>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
     <link rel="stylesheet" href="assets/css/style.css" type="text/css"/>
-	<style>
-	.col-md-12
-	{
-	width: 40%;
-    margin-left: 32%;
-	}
-	</style>
 </head>
 <body>
 
